002f0a1cfef53c74257f83c783f5b28ffca2e7f1
Jessica Wang <wangjessica02@gmail.com>
2020-03-02 13:31:55 -0500

    copied over isOrWillCollide

--- scripts/page.js
+++ scripts/page.js
@@ -94,21 +94,28 @@ function keydownRouter(e) {
 }
 
 function checkCollisions() {
-  
+  // TODO!
 }
 
-// Check if two objects are colliding
 function isColliding(o1, o2) {
-  const o1D = { 'left': o1.offset().left,
-        'right': o1.offset().left + o1.width(),
-        'top': o1.offset().top,
-        'bottom': o1.offset().top + o1.height()
-      };
+  return isOrWillCollide(o1, o2, 0, 0);
+}
+
+function willCollide(o1, o2, o1_xChange, o1_yChange){
+  return isOrWillCollide(o1, o2, o1_xChange, o1_yChange);
+}
+
+function isOrWillCollide(o1, o2, o1_xChange, o1_yChange){
+  const o1D = { 'left': o1.offset().left + o1_xChange,
+        'right': o1.offset().left + o1.width() + o1_xChange,
+        'top': o1.offset().top + o1_yChange,
+        'bottom': o1.offset().top + o1.height() + o1_yChange
+  };
   const o2D = { 'left': o2.offset().left,
         'right': o2.offset().left + o2.width(),
         'top': o2.offset().top,
         'bottom': o2.offset().top + o2.height()
-      };
+  };
   // Adapted from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
   if (o1D.left < o2D.right &&
     o1D.right > o2D.left &&
@@ -137,7 +144,7 @@ function createItemDivString(itemIndex, type, imageString){
 }
 
 function updateThrownItemPosition(elementObj, xChange, yChange, iterationsLeft){
-  
+  // TODO!
 }
 
 function graduallyFadeAndRemoveElement(elementObj){
@@ -156,7 +163,7 @@ function startParade(){
 
 // Handle player movement events
 function movePerson(arrow) {
-  
+  // TODO!
   switch (arrow) {
     case KEYS.left: { // left arrow
       let newPos = parseInt(player.css('left'))-PERSON_SPEED;
